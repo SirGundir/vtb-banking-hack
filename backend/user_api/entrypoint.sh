ea8cb7786a6fe082a30d6c029013e97cee7dd1dc
v-ampire <webjob010@gmail.com>
2025-10-31 17:05:31 +0300

    init

---
+++ backend/user_api/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+echo "Waiting for Postgres..."
+until nc -z vtb_banking.postgres 5432; do
+  sleep 1
+done
+
+echo "Waiting for Redis..."
+until nc -z vtb_banking.redis 6379; do
+  sleep 1
+done
+
+echo "Starting app..."
+python src/main.py
---
+++ backend/user_api/src/main.py
@@ -0,0 +1,11 @@
+import time
+
+
+def main():
+    while True:
+        print('user api')
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+    main()
