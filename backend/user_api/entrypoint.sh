76b869c4660025acc34cfa07ed15bf7c0d33b69f
v-ampire <webjob010@gmail.com>
2025-11-08 13:50:19 +0300

    fix run api

--- backend/user_api/entrypoint.sh
+++ backend/user_api/entrypoint.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 set -e
 
-wait_for_service() {
-  local name=$1
-  local host=$2
-  local port=$3
-
-  echo "Waiting for $name at $host:$port..."
-  until nc -z "$host" "$port" >/dev/null 2>&1; do
+wait_for_postgres() {
+  echo "Waiting for PostgreSQL at $POSTGRES_HOST:$POSTGRES_PORT..."
+  until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" >/dev/null 2>&1; do
     sleep 1
   done
-  echo "$name is up!"
+  echo "PostgreSQL is up!"
 }
 
-wait_for_service "PostgreSQL" "$POSTGRES_HOST" "$POSTGRES_PORT"
-wait_for_service "Redis" "$REDIS_HOST" "$REDIS_PORT"
-wait_for_service "Kafka" "$KAFKA_HOST" "$KAFKA_PORT"
+wait_for_redis() {
+  echo "Waiting for Redis at $REDIS_HOST:$REDIS_PORT..."
+  until redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" ping >/dev/null 2>&1; do
+    sleep 1
+  done
+  echo "Redis is up!"
+}
+
+wait_for_kafka() {
+  echo "Waiting for Kafka at $KAFKA_HOST:$KAFKA_PORT..."
+  # просто TCP + доп. задержка
+  until nc -z "$KAFKA_HOST" "$KAFKA_PORT" >/dev/null 2>&1; do
+    sleep 1
+  done
+  echo "Kafka TCP port is open, waiting extra 10s..."
+  sleep 10
+}
+
+wait_for_postgres
+wait_for_redis
+wait_for_kafka
 
 python3 /src/main.py run_server
